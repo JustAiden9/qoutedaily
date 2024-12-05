e17baf70885ee9837594600e7cc27d97e3a1dc68
Aiden Baker <aidenbak9@gmail.com>
2024-12-05 09:27:28 -0600

    Initial Commit

---
+++ qoutedaily/qoutedailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  qoutedailyApp.swift
+//  qoutedaily
+//
+//  Created by Aiden Baker on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct qoutedailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ qoutedailyTests/qoutedailyTests.swift
@@ -0,0 +1,16 @@
+//
+//  qoutedailyTests.swift
+//  qoutedailyTests
+//
+//  Created by Aiden Baker on 12/5/24.
+//
+
+import Testing
+
+struct qoutedailyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
